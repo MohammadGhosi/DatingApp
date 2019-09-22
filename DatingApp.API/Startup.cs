aad6a83f7532efe3067390caa4e99258416682cb
Mohammad Ashfaque <mohammad.development@gmail.com>
2019-09-22 23:23:43 +0530

    End of Section 8 Updated

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -12,27 +12,32 @@ using System.Net;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using DatingApp.API.Helpers;
+using AutoMapper;
 
 namespace DatingApp.API
 {
     public class Startup
     {
+        public IConfiguration Configuration{get;}
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
-        
-        public IConfiguration Configuration{get;}
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             var constr = Configuration.GetConnectionString("DefaultConnection");
             services.AddDbContext<DataContext>(Options => Options.UseSqlite(constr));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling = 
+                    Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddCors();
+            services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddScoped<IAuthRepository, AuthRepository>();
+            services.AddScoped<IDatingRepository,DatingRepository>();
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options => {
                     options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
